68fdae0b9b11d6dbc8dba477421d066fc4ae6513
徐林森 <744940901@qq.com>
2018-11-18 15:18:25 +0800

    🐸V3EX User Module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const Koa = require('koa');
 const app = new Koa();
+const session = require('koa-session');
+const bodyParser = require('koa-bodyparser')();
 const db = require('./modules/db.js');
-
 const router = require('./routes');
 
+// koa-session
+app.keys = ['helloworld_v3ex'];
+app.use(session({
+  key: 'koa:sess',
+  maxAge: 86400000
+}, app));
+
+app.use(bodyParser);
+app.use(require('./middlewares/filter.js'));
+app.use(require('./middlewares/response.js'));
 app.use(router.routes());
 app.listen(3000);
---
+++ src/modules/user/user.js
@@ -0,0 +1,21 @@
+let mongooes = require('mongoose');
+
+/**
+ * 用户结构
+ * -- groupId: 用户组 (0: 普通用户)
+ * -- username: 用户名
+ * -- password: 密码(md5加盐加密)
+ * -- email: 注册邮箱
+ * -- createAt: 注册日期
+ */
+const userSchema = new mongooes.Schema({
+  groupId: { type: Number, default: 0 },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, default: '' },
+  createAt: { type: Date, required: true }
+});
+
+const User = mongooes.model('User', userSchema);
+
+module.exports = User;
